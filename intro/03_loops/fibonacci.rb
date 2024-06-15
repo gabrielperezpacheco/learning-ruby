3ed9bd2d1ed965778d2b0ec00d38749dbaedb5c4
Kyup <gabriel.perez.pacheco@outlook.es>
2024-06-15 06:11:16 -0600

    fibonacci solve

--- intro/03_loops/fibonacci.rb
+++ intro/03_loops/fibonacci.rb
@@ -1,4 +1,16 @@
 # Print out all the Fibonacci numbers from 1 to 10 in order
+# oii
+puts "0, 1, 1, 2, 3, 5, 8, 13, 21, 34"
 
 # Your code goes here
+last = 0
+current = 1
 
+for i in 0..7
+  aux = current 
+  current += last 
+  last = aux
+
+  puts " #{ current }"
+
+end
