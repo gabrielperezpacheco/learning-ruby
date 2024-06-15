ae570f109404ec85008cc2399ef804d6c8021f0a
Kyup <gabriel.perez.pacheco@outlook.es>
2024-06-15 05:58:03 -0600

    names exercise

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -5,4 +5,13 @@ name3 = "Beric Dondarrion"
 name4 = "Melisandre"
 
 # Your code goes here
+avg_length = (name1.length + name2.length + name3.length + name4.length) / 4
 
+puts "Whats you'r name?"
+my_name = gets.chomp
+
+if(my_name.length < avg_length)
+  puts "#{ my_name } is shorter than average"
+else 
+  puts "#{ my_name } is longer than average"
+end
